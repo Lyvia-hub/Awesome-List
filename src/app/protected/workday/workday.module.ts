499c807004e2eb648722f246e27c58fa3f125ed7
Lyvia Blay <lyviablay@gmail.com>
2019-10-22 17:53:45 -0400

    Initialisation of a complex form (WorkdayForm)

--- src/app/protected/workday/workday.module.ts
+++ src/app/protected/workday/workday.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { WorkdayComponent } from './workday/workday.component';
 import { WorkdayRoutingModule } from './workday-routing.module';
+import { WorkdayFormComponent } from './workday-form/workday-form.component';
+import { WorkdayFormDateComponent } from './workday-form-date/workday-form-date.component';
+import { WorkdayFormTasksComponent } from './workday-form-tasks/workday-form-tasks.component';
+import { WorkdayFormTasksItemComponent } from './workday-form-tasks-item/workday-form-tasks-item.component';
+import { WorkdayFormTasksNotesComponent } from './workday-form-tasks-notes/workday-form-tasks-notes.component';
 
 
 
 @NgModule({
-  declarations: [WorkdayComponent],
+  declarations: [WorkdayComponent, WorkdayFormComponent, WorkdayFormDateComponent, WorkdayFormTasksComponent, WorkdayFormTasksItemComponent, WorkdayFormTasksNotesComponent],
   imports: [
     SharedModule,
     WorkdayRoutingModule
