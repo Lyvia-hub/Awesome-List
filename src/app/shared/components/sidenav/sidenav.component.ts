a8d45a8b3a3a00450fdb1fd0c152ffe43ab05b2d
Lyvia Blay <lyviablay@gmail.com>
2019-11-27 18:41:10 -0400

    Adaption of the interface according to user role

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../../models/user';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/core/services/auth.service';
+
 @Component({
   selector: 'al-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent implements OnInit, OnDestroy {
 
   public prefix = 'app';
   public dashboardPath = `${this.prefix}/dashboard`;
@@ -13,10 +17,21 @@ export class SidenavComponent implements OnInit {
   public workdayPath = `${this.prefix}/workday`;
   public profilPath = `${this.prefix}/profil`;
   public parametersPath = `${this.prefix}/parameters`;
+  public subscription: Subscription;
+  public user: User;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private authService: AuthService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscription = this.authService.user$.subscribe(user =>
+      this.user = user
+      );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
   public navigate(page: string): void {
     this.router.navigate([page]);
@@ -24,5 +39,7 @@ export class SidenavComponent implements OnInit {
   public isActive(page: string): boolean {
     return this.router.isActive(page, true);
   }
+
+
 
 }
